be8af34560d445e01c363f86b3141e03896538c6
andyfullstack <andriikoliadenko@gmail.com>
2023-10-06 20:40:15 +0300

    upd

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -18,6 +18,7 @@ const markup = galleryItems
   })
   .join("");
 picElements.insertAdjacentHTML("beforeend", markup);
+
 const modalOpen = evt => {
   evt.preventDefault();
   if (!evt.target.classList.contains("gallery__image")) {
@@ -25,6 +26,7 @@ const modalOpen = evt => {
   }
   const clickedImageAlt = evt.target.getAttribute("alt");
   const clickedImageSrc = evt.target.dataset.source;
+
   const instance = basicLightbox.create(
     `<img src='${clickedImageSrc}' alt='${clickedImageAlt}'/>`,
     {
@@ -33,27 +35,50 @@ const modalOpen = evt => {
       },
       onClose: instance => {
         document.removeEventListener("keydown", modalClose);
-        instance = null;
+        // instance = null;
+
         //    instance.element().querySelector("a").onclick = instance.close;
       },
     }
   );
+
   instance.show();
 };
+
 const modalClose = evt => {
   if (evt.key !== "Escape") {
+    console.log(evt);
     return;
-  } else {
-    instance.close();
   }
+  instance.close();
 };
-//   if (evt.key === "Escape" && modalOpen()) {
-//     instance.close();
-//     modalClose = true;
-//   }
-// };
 
 document.addEventListener("keydown", modalClose);
 picElements.addEventListener("click", evt => {
   modalOpen(evt);
 });
+
+// let modalIsOpen = false;
+// let modal;
+// function onImageClick(event) {
+//   event.preventDefault();
+//   const target = event.target;
+
+//   if (target.classList.contains("gallery__image")) {
+//     const originalImageUrl = target.dataset.source;
+//     modal = basicLightbox.create(
+//       `<img src="${originalImageUrl}" width="800" height="600"> `
+//     );
+//     modal.show();
+//     modalIsOpen = true;
+//   }
+//   document.addEventListener("keydown", keyDown);
+// }
+
+// function keyDown(event) {
+//   if (event.key === "Escape" && modalIsOpen) {
+//     modal.close();
+//     modalIsOpen = false;
+//     document.removeEventListener("keydown", keyDown);
+//   }
+// }
